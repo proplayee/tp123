32d154e9e10c1c52f5d71c68354846b0a06d4768
proplayee <99133789+proplayee@users.noreply.github.com>
2022-02-09 00:11:10 +0700

    Add files via upload

---
+++ CONF_FILE/ninja.sh
@@ -0,0 +1,17 @@
+#!/data/data/com.termux/files/usr/bin/bash
+#KhanhNguyen9872
+cd ~/../usr/share/KhanhNguyen9872 2> /dev/null
+unset LD_PRELOAD
+command="proot"
+command+=" --link2symlink"
+command+=" -0"
+command+=" -r KhanhNguyen9872"
+command+=" -b /proc"
+command+=" -w /root"
+command+=" /usr/bin/env -i"
+command+=" HOME=/root"
+command+=" PATH=/usr/local/sbin:/usr/local/bin:/bin:/usr/bin:/sbin:/usr/sbin:/usr/games:/usr/local/games"
+command+=" TERM=$TERM"
+command+=" LANG=C.UTF-8"
+command+=" /bin/bash --login"
+$command -c "khanh"
